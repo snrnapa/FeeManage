4dd861208c59cff125e1332536e3949a1b648066
snrnapa <shino.satoru@gmail.com>
2023-06-18 15:13:05 +0900

    worker処理について、jpa化けるによって不要となった箇所を削除した。

--- src/main/java/com/napa/app/service/WorkerService.java
+++ src/main/java/com/napa/app/service/WorkerService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.napa.app.Repository.WorkerRepository;
-import com.napa.app.dao.WorkerDao;
 import com.napa.app.entity.Worker;
 
 @Service
@@ -16,50 +15,32 @@ public class WorkerService {
 	
 	@Autowired
 	private WorkerRepository workerrepository;
-//	WorkerDao workerdao;
 
+
+
+	public List<Worker> AllWorkerSelect(){
+		return workerrepository.findAll();
+	}
 	
-
-	////JPA使用前
-	//public List<Worker> AllWorkerSelect(){
-	//	return this.workerdao.AllWorker();
-	//}
 	
-//JPA使用バージョン
-public List<Worker> AllWorkerSelect(){
-	return workerrepository.findAll();
-}
-
-
-	////JPA未使用タイプ
-	//public Worker OneWorkerSelect(Integer id){
-	//	return this.workerdao.OneWorker(id);
-	//}
-
-//JPA使用タイプ
-public Optional<Worker> OneWorkerSelect(@PathVariable("id") Integer id){
-	return workerrepository.findById(id);
-//	return this.workerdao.OneWorker(id);
-}
-
-
-public void AddWorker(Integer id,String first_name,String last_name,String dept,String team){
-	Worker worker = new Worker();
-	worker.setId(id);
-	worker.setFirst_name(first_name);
-	worker.setLast_name(last_name);
-	worker.setDept(dept);
-	worker.setTeam(team);
-	workerrepository.save(worker);
-//	workerdao.WorkerAdd(worker);
-}
-
-public void Deleteworker(@PathVariable("id") Integer id){
-
-//	Optional<Worker> worker = workerrepository.findById(id);
-	workerrepository.deleteById(id);
-//	workerdao.Deleteworker(worker);
-}
-
+	public Optional<Worker> OneWorkerSelect(@PathVariable("id") Integer id){
+		return workerrepository.findById(id);
+	}
+	
+	
+	public void AddWorker(Integer id,String first_name,String last_name,String dept,String team){
+		Worker worker = new Worker();
+		worker.setId(id);
+		worker.setFirst_name(first_name);
+		worker.setLast_name(last_name);
+		worker.setDept(dept);
+		worker.setTeam(team);
+		workerrepository.save(worker);
+	}
+	
+	public void Deleteworker(@PathVariable("id") Integer id){
+	
+		workerrepository.deleteById(id);
+	}
 
 }
