f0dbd346251b7a84e145b98af3b121604b6409ed
snrnapa <shino.satoru@gmail.com>
2023-07-08 16:36:30 +0900

    DBのデータを少し減らしたよ！！

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,9 @@ insert into worker ( id, first_name, last_name, dept, team ) VALUES ( '4', 'uyu'
 
 insert into worker ( id, first_name, last_name, dept, team ) select id + (select max(id) from worker), 'pepepe', 'pepepe', 'HHB', 'UUY' from worker;
 insert into worker ( id, first_name, last_name, dept, team ) select id + (select max(id) from worker), 'pepepe', 'pepepe', 'HHB', 'UUY' from worker;
-    
+
+
+
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '182','2023-06-27' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '185','2023-06-28' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '188','2023-06-29' );
@@ -15,26 +17,6 @@ insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1'
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '194','2023-07-01' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '197','2023-07-02' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '200','2023-07-03' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '203','2023-07-04' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '206','2023-07-05' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '209','2023-07-06' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '212','2023-07-07' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '215','2023-07-08' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '218','2023-07-09' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '221','2023-07-10' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '224','2023-07-11' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '227','2023-07-12' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '230','2023-07-13' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '233','2023-07-14' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '236','2023-07-15' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '239','2023-07-16' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '242','2023-07-17' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '245','2023-07-18' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '248','2023-07-19' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '251','2023-07-20' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '254','2023-07-21' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '257','2023-07-22' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '260','2023-07-23' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '263','2023-07-24' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '266','2023-07-25' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '269','2023-07-26' );
@@ -45,68 +27,25 @@ insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1'
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '284','2023-07-31' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '287','2023-08-01' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '290','2023-08-02' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '293','2023-08-03' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '296','2023-08-04' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '299','2023-08-05' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '302','2023-08-06' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '305','2023-08-07' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '308','2023-08-08' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '311','2023-08-09' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '314','2023-08-10' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '317','2023-08-11' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '320','2023-08-12' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '323','2023-08-13' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '326','2023-08-14' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '329','2023-08-15' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '332','2023-08-16' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '335','2023-08-17' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '338','2023-08-18' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '341','2023-08-19' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '344','2023-08-20' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '347','2023-08-21' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '350','2023-08-22' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '353','2023-08-23' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '356','2023-08-24' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '359','2023-08-25' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '362','2023-08-26' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '365','2023-08-27' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '368','2023-08-28' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '371','2023-08-29' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '374','2023-08-30' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '377','2023-08-31' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '380','2023-09-01' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '383','2023-09-02' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '386','2023-09-03' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '389','2023-09-04' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '392','2023-09-05' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '395','2023-09-06' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '398','2023-09-07' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '401','2023-09-08' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '404','2023-09-09' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '407','2023-09-10' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '410','2023-09-11' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '413','2023-09-12' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '416','2023-09-13' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '419','2023-09-14' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '422','2023-09-15' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '425','2023-09-16' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '428','2023-09-17' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '431','2023-09-18' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '434','2023-09-19' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '437','2023-09-20' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '440','2023-09-21' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '443','2023-09-22' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '446','2023-09-23' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '449','2023-09-24' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '452','2023-09-25' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '455','2023-09-26' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '458','2023-09-27' );
-insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '461','2023-09-28' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '464','2023-09-29' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '467','2023-09-30' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '470','2023-10-01' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '473','2023-10-02' );
 insert into fee ( id, fee_seq,round_trip, total_fee, use_date) VALUES ( '1', '1', 'yes', '476','2023-10-03' );
+
 
 
 
