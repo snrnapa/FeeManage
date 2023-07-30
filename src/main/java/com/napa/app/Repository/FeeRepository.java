a1a239d33b58bdaf7373dfeed85ff7b382187f28
snrnapa <shino.satoru@gmail.com>
2023-07-30 20:56:13 +0900

    不要なライブラリを削除した

--- src/main/java/com/napa/app/Repository/FeeRepository.java
+++ src/main/java/com/napa/app/Repository/FeeRepository.java
@@ -1,8 +1,6 @@
 package com.napa.app.Repository;
 
-import java.sql.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,11 +10,9 @@ import org.springframework.stereotype.Repository;
 import com.napa.app.entity.Fee;
 import com.napa.app.entity.FeePK;
 
-
 @Repository
-public interface FeeRepository extends JpaRepository<Fee, FeePK>{
-	 @Query("select o from Fee o where o.id = :id")
-    List<Fee> findByPrimarykeys(@Param("id")int id);
-	
+public interface FeeRepository extends JpaRepository<Fee, FeePK> {
+    @Query("select o from Fee o where o.id = :id")
+    List<Fee> findByPrimarykeys(@Param("id") int id);
 
 }
